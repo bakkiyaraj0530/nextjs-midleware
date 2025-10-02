97503c67156efe6eb2b6243122f20c69ecb4dd4e
bakkiyaraj <sanjay0530@yahoo.co.in>
2025-10-02 22:10:19 +0530

    useeffect added in contact us

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Typography } from '@mui/material';
 
 export default function Contact() {
+   useEffect(() => {
+     alert('Contact us')
+   }, [])
   return (
     <div style={{ padding: 20 }}>
       <Typography variant="h4">Contact Page</Typography>
